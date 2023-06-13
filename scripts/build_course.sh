ebd7e4f23c3287075b2866b7caa29fc94ae8166b
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2023-06-13 19:34:49 +0300

    Смена структуры репозитория (#6)
    * Moved files for LFD113x to own folder
    
    * Removed common info from README of the LFD113x
    
    * Added scripts for building multiple courses
    
    * Added README for the repository
    
    * Fixed path to script in release workflow
    
    * Fixed tag parsing in release scripts

---
+++ scripts/build_course.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+
+BASEDIR=$(dirname "$(realpath "$0")")
+ROOTDIR=$(realpath "$BASEDIR/..")
+
+function usage() {
+    cat <<tac
+Usage: $0 [OPTION] COURSE_NAME
+
+Build artifacts of the course with COURSE_NAME
+  -o, --out FILENAME       output pdf FILENAME
+  -h, --help               print this help message and exit
+
+Examples:
+  $0 LFD113x-RU
+  $0 -o 'Инструментарий_и_компиляторные_оптимизации_для_RISC-V_(LFD113x)_RU.pdf' LFD113x-RU
+tac
+}
+
+
+# Parse arguments
+COURSEDIR=""
+MAKEOPTS=()
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -o|--out)
+            MAKEOPTS+=("RESULT_PDF=$2")
+            shift 2
+            ;;
+        -*)
+            echo "Unknown option $1"
+            usage
+            exit 1
+            ;;
+        *)
+            if [ -d "$ROOTDIR/$1" ] ; then
+                COURSEDIR=$(realpath "$ROOTDIR/$1")
+                break
+            else
+                echo "Unknown course $1"
+                usage
+                exit 1
+            fi
+            ;;
+    esac
+done
+
+if [ "$COURSEDIR" == "" ]; then
+    echo "No course selected"
+    usage
+    exit 1
+fi
+
+# Build selected course
+make -C "$COURSEDIR" "${MAKEOPTS[@]}"
